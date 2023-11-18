16ec8ad76daaac399995fa530ead767c3ee3ca57
brainstealer <v.i.kovalenko2020@gmail.com>
2023-11-18 15:09:19 +0300

    new project

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [userName, setUserName] = useState('');
+    const [userSecondName, setUserSecondName] = useState('');
+    const [userList, setUserList] = useState([]);
+    const addUser = () => {
+        if (userName && userSecondName) {
+            const newUser = {
+                id: Date.now(),
+                userName: userName,
+                userSecondName: userSecondName,
+            
+            }
+            setUserList([...userList, newUser]);
+        }
+        setUserName('');
+        setUserSecondName('');
+    }
+
+    const clearInput = () => {
+        setUserName('');
+        setUserSecondName('');
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={newText => setUserName(newText)}
+                    placeholder='First name'
+                    defaultValue={userName}
+                    />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={newText => setUserSecondName(newText)}
+                    placeholder='Second name'
+                    defaultValue={userSecondName}
+                    />
+                
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button 
+                    title='Clear'
+                    onPress={clearInput}
+                    />
+                <Button 
+                    title='Add user'
+                    onPress={addUser}
+                    />
+            </View>
+            <View style={styles.userList}>
+                    <FlatList
+                        keyExtractor={(item) => item.id}
+                        data={userList}
+                        renderItem={({ item }) => 
+                            <Text>{item.userName} {item.userSecondName}</Text>
+                        }
+                    />
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        paddingTop: 50,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        rowGap: 5,
+    },
+    inputContainer: {
+        flexDirection: 'column',
+        width: '90%',
+        rowGap: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 20,
+        padding: 5,
+
+
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        columnGap: 5,
+    },
+    userList: {
+        height: '70%'
+    }
 });
